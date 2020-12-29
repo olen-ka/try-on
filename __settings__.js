1b791d699812ea7d386e0795f79aaf0c13fcd1cc
Olena Kuzii <okuziy@gmail.com>
2020-12-29 12:03:25 +0200

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1054334.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 40415686, 40465012 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/40418098/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/40418099/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/40418097/1/ammo.js', 'preload' : true},
+];
